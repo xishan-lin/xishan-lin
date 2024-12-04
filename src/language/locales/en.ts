1a49f9e585cb8f54897fcb1431cd1c4e3613bef6
linxiang <lionsom.linx@qq.com>
2024-12-04 16:59:28 +0800

    init

---
+++ src/language/locales/en.ts
@@ -0,0 +1,12 @@
+const en = {
+  title: 'XiShan',
+  navi: {
+    home: 'HomePage',
+    books: 'BOOKS',
+  },
+  common: {
+    changeLanguage: 'change Language',
+  }
+}
+
+export default en;
---
+++ src/views/pc/PDFAllView/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  name: 'PDFAllView'
+}
+</script>
+
+<script setup lang="ts">
+import PdfAllViewer from '@/components/PdfAllViewer.vue'
+</script>
+
+<template>
+  <PdfAllViewer />
+</template>
+
+
+<style scoped></style>
