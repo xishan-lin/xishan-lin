a19b1b32642158360360b5a295ceb4f19169c8c1
linxiang <lionsom.linx@qq.com>
2025-05-14 17:34:35 +0800

    族谱下载引导页

---
+++ src/views/family-tree/downloadGuide/DownloadGuide.vue
@@ -0,0 +1,12 @@
+<!-- 族谱下载 -->
+<template>
+  <div class="download-guide">
+    <h1>族谱下载指南</h1>
+
+    莆田林氏西山本支族谱（总） 链接: https://pan.baidu.com/s/1x73EgtX2MWnNSxH8Bo__7Q 密码: 6nu5
+    莆田林氏西山本支族谱（卷一） 链接: https://pan.baidu.com/s/1sU37qwIDV_WG_A5sOLOVgw 密码: 7lt6
+    莆田林氏西山本支族谱（卷二） 链接: https://pan.baidu.com/s/1-m8OJKE8ijPlBfzzM3z7wg 密码: mbjm
+    莆田林氏西山本支族谱（卷三） 链接: https://pan.baidu.com/s/1zuo6-noPIodd-_9AR0CE1g 密码: 9ji2
+    莆田林氏西山本支族谱（卷四） 链接: https://pan.baidu.com/s/1VTKImcqlIC2N44mHpbbpjQ 密码: hi1h
+  </div>
+</template>
